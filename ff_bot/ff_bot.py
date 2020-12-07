f3db5fc6b87f32ed21e57e91cdd38a62efd6debc
Aaron <kahanam24@gmail.com>
2020-12-07 07:24:54 -0600

    fix

--- ff_bot/ff_bot.py
+++ ff_bot/ff_bot.py
@@ -409,7 +409,7 @@ if __name__ == '__main__':
         day_of_week='sun', hour=20, minute=45, start_date=ff_start_date, end_date=ff_end_date,
         timezone=game_timezone, replace_existing=True)
     sched.add_job(bot_main, 'cron', ['get_final'], id='final',
-        day_of_week='tue', hour=8, minute=30, start_date=ff_start_date, end_date=ff_end_date,
+        day_of_week='wed', hour=8, minute=30, start_date=ff_start_date, end_date=ff_end_date,
         timezone=my_timezone, replace_existing=True)
     #sched.add_job(bot_main, 'cron', ['get_scoreboard_short'], id='scoreboard1',
         #day_of_week='fri', hour=8, minute=30, start_date=ff_start_date, end_date=ff_end_date,
